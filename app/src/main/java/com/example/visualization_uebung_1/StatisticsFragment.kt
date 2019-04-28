0e355f3a06ba6b7f23c3a04136f76e9b6004bd88
EKo Sup <e.supardjo@gmx.de>
2019-04-28 17:45:45 +0200

    changed gitignore and unused import
    text

--- app/src/main/java/com/example/visualization_uebung_1/StatisticsFragment.kt
+++ app/src/main/java/com/example/visualization_uebung_1/StatisticsFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.fragment_statistics.*
 import kotlinx.android.synthetic.main.fragment_statistics.view.*
 
 class StatisticsFragment :Fragment(){
